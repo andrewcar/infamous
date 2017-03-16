e83209e027efdedfeadfc6a21c4685477b731dde
appdrew <andrew@appdrew.com>
2017-03-16 19:06:58 -0400

    Set table view separator color to clear

--- Infamous/PlaylistTVC.swift
+++ Infamous/PlaylistTVC.swift
@@ -15,6 +15,7 @@ class PlaylistTVC: UITableViewController {
         
         navigationController?.navigationBar.barTintColor = .black
         tableView.rowHeight = 88
+        tableView.separatorColor = .clear
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -32,23 +33,20 @@ class PlaylistTVC: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
     
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
